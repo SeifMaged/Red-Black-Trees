144251268c355bb4ff1cc4aef84fe133a1711e82
RowanAhmed10 <roroahmed1142@gmail.com>
2024-04-18 15:52:39 +0200

    implement search

--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -5,14 +5,55 @@ public class RedBlackTree {
         this.root = root;
     }
 
-    public int getBlackHeight(){
+    public boolean search(String key) {
+        return search(key, root);
+    }
+
+    private boolean search(String key, TreeNode root) {
+        if (root == null) {
+            return false;
+        }
+        int result = key.compareToIgnoreCase(root.data);
+
+        if (result == 0) {
+            return true;
+        }
+        if (result < 0) {
+            return search(key, root.left);
+        }
+        return search(key, root.right);
+    }
+
+    public int getBlackHeight() {
         return getBlackHeight(root, 0);
     }
-    private int getBlackHeight(TreeNode root, int blackHeight){
-        if(root != null){
-           return (root.isRed) ? getBlackHeight(root.left, blackHeight) : getBlackHeight(root.left, blackHeight + 1);
+
+    private int getBlackHeight(TreeNode root, int blackHeight) {
+        if (root != null) {
+            return (root.isRed) ? getBlackHeight(root.left, blackHeight) : getBlackHeight(root.left, blackHeight + 1);
         }
         return blackHeight;
     }
 
+    public int getHeight() {
+        return getHeight(root);
+    }
+
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+    }
+
+    public int getSize() {
+        return getSize(root);
+    }
+
+    private int getSize(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + getSize(root.left) + getSize(root.right);
+    }
 }
