df5d6e468d244a95ba6ca394220748ce13c3dda4
RowanAhmed10 <roroahmed1142@gmail.com>
2024-04-20 00:06:21 +0200

    deleted extra code

--- src/Main.java
+++ src/Main.java
@@ -9,31 +9,5 @@ public class Main {
         dict.insertWord();
         dict.insertWord();
         dict.lookUp();
-
-        // RedBlackTree tree = new RedBlackTree();
-        // tree.insert("a");
-        // System.out.println("root is " + tree.getRoot().getData());
-        // tree.insert("b");
-        // System.out.println("root is " + tree.getRoot().getData());
-        // tree.insert("c");
-        // System.out.println("root is " + tree.getRoot().getData());
-        // System.out.println("black height is " + tree.getBlackHeight());
-        // System.out.println("height is " + tree.getHeight());
-        // System.out.println("===============================================================");
-
-        // tree.insert("v");
-        // System.out.println("root is " + tree.getRoot().getData());
-        // System.out.println("black height is " + tree.getBlackHeight());
-        // tree.insert("w");
-        // tree.insert("x");
-        // tree.insert("y");
-        // tree.insert("z");
-        // System.out.println("root is " + tree.getRoot().getData());
-        // System.out.println("black height is " + tree.getBlackHeight());
-
-        // tree.printAll();
-        // if(tree.search("a")){
-        // System.out.println(tree.getNode("a").getData());
-        // }
     }
 }
